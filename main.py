547d2e845a1795c3d1f06d83d2cab0a50201b45d
Nathan Skwarek <nskwarek@eugeniaschool.com>
2025-10-02 13:12:21 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+# TODO: Create an integer variable named 'age' with your age
+# TODO: Create a float variable named 'height' with your height in meters
+# TODO: Create a string variable named 'name' with your name
+# TODO: Create a boolean variable named 'is_student' and set it to True
+# Print all variables and their types
+# Example: print(f"Age: {age}, Type: {type(age)}")
+
+# age = 22
+# height = 1.88
+# name = "Nathan"
+# #is_student = True
+
+#print(f"Age: {age}, Type: {type(age)}")
+#print(f"Taille: {height}, Type: {type(age)}")
+#print(f"Nom: {name}, Type {type(name)}")
+#print(f"es un etudiant?: {is_student}, Type: {type(is_student)}")
+
+# a = 15
+# b = 4
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# print(a % b)
+# print(f"a+b={a+b}")
+# print(f"a puissance b={a**b}")
+
+# TODO: Create two string variables 'first_name' and 'last_name'
+# TODO: Concatenate the two names to create a 'full_name' variable
+# TODO: Print the full name in uppercase
+# TODO: Print the length of the full name
+# TODO: Create a string with multiple words and split it into a list
+
+# first_name = "Nathan"
+# last_name = "Skwarek"
+# full_name = f"{first_name} {last_name}"
+# print(full_name.upper())
+# print(len(full_name))
+# phrase = "Bonjour je suis nathan"
+# mots = phrase.split()
+# print(mots)
+
+# TODO: Convert a string containing a number to an integer
+# TODO: Convert a float to an integer
+# TODO: Convert an integer to a float
+# TODO: Convert a number (integer or float) to a string
+# Print all converted values and their new types
+
+# nombre_text = "20"
+# nombre_entier = int(nombre_text)
+# print(nombre_entier)
+
+# nombre_text_float = 0.2
+# nombre_entier_float = int(nombre_text_float)
+# print(nombre_entier_float)
+
+# print(type(nombre_entier_float))
+
+# chiffre_en_int = 9
+# chiffre_en_string = str(chiffre_en_int)
+
+# print(f"chiffre en string = {type(chiffre_en_string)}")
+
+
+
+# TODO: Create two boolean variables
+# TODO: Perform AND, OR, and NOT operations on these variables
+# Print the results
+# TODO: Create two numeric variables and use comparison operators
+# (>, <, >=, <=, ==, !=) to compare them
+# Print the results of these comparisons
+
+
+is_a_man = True
+is_alive = True
